028e0035b13b65be1078a3a735c300651baf100f
Gokul0201 <kgokula314@gmail.com>
2022-12-08 18:53:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { CookiesProvider } from "react-cookie";
 import './App.css';
+import Schedular from "./components/Scheduler";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CookiesProvider>
+      <div className="App">
+        <h1>Manual Scheduler</h1>
+        <Schedular />
+    
+      </div>
+    </CookiesProvider>
   );
 }
 
-export default App;
